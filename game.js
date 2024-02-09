4d38dcc73e33c9e5ca1fbd13f30983523d5a2902
Lisa Szymanski <43975456+battleshipcats@users.noreply.github.com>
2024-02-09 12:58:48 -0500

    Initial commit

---
+++ game.js
@@ -0,0 +1,99 @@
+
+var buttonColors = ["green","red","yellow","blue"];
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = (0);
+
+// click any key to start game.
+$(document).keypress(function() {
+    if (!started) {
+    nextSequence();
+    started = true;
+    };
+ }); 
+
+ // trigger next sequence
+function nextSequence() {
+    level++;
+    userClickedPattern = [];
+    $("#level-title").html("<br>" + "Level "+level);
+    var randomNumber = Math.floor(Math.random() * 4);   
+    randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+    //console.log("Game: "+gamePattern);  //<-- this console log is for testing only.
+};
+
+// button clicks
+$(".btn").on("click", function() {
+    var userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    animatePress(userChosenColor);  
+    playSound(userChosenColor);
+    var lastUserIndex = userClickedPattern.length-1; //this will always set back to 0 after next sequence.
+    //console.log("User: "+userClickedPattern);  //<-- this console log is for testing only.
+    checkAnswer(lastUserIndex);
+    });
+
+// button animations
+function animatePress (currentColor) {
+    var activeButton = $("#" + currentColor);
+    $(activeButton).addClass("pressed");
+    setTimeout(function() {
+        $(activeButton).removeClass("pressed");
+    }, 100);
+};
+
+//button sounds
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3")
+    audio.play();
+};
+
+//check answer
+ function checkAnswer(currentLevel) {
+    if(userClickedPattern[(currentLevel)] === gamePattern[(currentLevel)]) {
+    //console.log("Success "+currentLevel);  //<-- currentlevel is for testing only
+        if (currentLevel === gamePattern.length-1) {
+            if (level<10) {
+                setTimeout (function() {                
+                nextSequence()
+            },1000); 
+        } else {
+            $("body").addClass("ten-of-ten");
+            setTimeout(function() {
+                $("body").removeClass("ten-of-ten");
+                $("#level-title").html("10 out of 10! <br> Press Any Key to Restart").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+                playSound("win");
+            }, 400);
+
+            startOver(); 
+            }
+        }
+
+    } else {
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+            $("#level-title").html("Game Over <br> Press Any Key to Restart");
+            playSound("wrong");
+        }, 200);
+
+        startOver();
+  }
+ };
+
+ function startOver() {
+    gamePattern = [];
+    started = false;
+    level = (0);
+ }
+
+//  if (level===3) {
+//     $("#level-title").text("10 out of 10! - Press Any Key to Restart");
+//     setTimeout (function() {                
+//         startOver()
+//     },1000); 
+// }
